4d40f5e608136cb49e929c5696e937ff57f4c751
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-03-31 14:53:23 -0500

    Assignment complete

--- script.js
+++ script.js
@@ -6,10 +6,38 @@ const MAX_DURATION = 5000;
 const snowflakesContainer = document.getElementById("snowflakes-container");
 const snowVSrain = document.querySelector("#snow-vs-rain");
 const quantity = document.querySelector("#quantity");
-const snowColor = document.querySelector("click-color");
+const snowColor = document.querySelector("#click-color");
+const wind = document.querySelector("#wind");
+let quantityAmount = 200;
 
 
-setInterval(() => createSnowflake(), 500);
+
+let quantityIncrease =  setInterval(() => createSnowflake(), quantityAmount);
+
+
+quantity.addEventListener("input", () => {
+  switch (quantity.value){
+    case "0":
+      quantityAmount = 99999999999;
+      break;
+    case "1":
+      quantityAmount = 200;
+      break;
+    case "2":
+      quantityAmount = 100;
+      break;
+    case "3":
+      quantityAmount = 65;
+      break;
+    case "4":
+      quantityAmount = 50;
+      break;
+    }
+    clearInterval(quantityIncrease)
+    quantityIncrease =  setInterval(() => createSnowflake(), quantityAmount);
+
+  }
+)
 
 function randint(lo, hi) {
   return Math.random() * (hi - lo) + lo;
@@ -23,6 +51,9 @@ function randomIcon() {
   }
 }
 
+snowColor.addEventListener("click", () => {
+  let color = color.value
+})
 function createSnowflake() {
   const snowFlake = document.createElement("i");
 
@@ -30,13 +61,15 @@ function createSnowflake() {
   snowFlake.style.left = randint(0, 100) + "%";
   snowFlake.style.opacity = Math.random();
   snowFlake.style.fontSize = randint(MIN_SIZE, MAX_SIZE) + "px";
+  snowFlake.style.color = snowColor.value;
  
 
   snowflakesContainer.appendChild(snowFlake);
+  
 
   snowFlake
     .animate(
-      { transform: `translate(0vw, 100vh)` },
+      { transform: `translate(${wind.value}vw, 100vh)` },
       { duration: randint(MIN_DURATION, MAX_DURATION) }
     )
     .finished.then(() => snowFlake.remove());
